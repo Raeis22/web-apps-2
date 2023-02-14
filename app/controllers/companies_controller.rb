dd0adef0af26bc4e63885a8bb9d1c0a46d7aa3f7
Raeis22 <ruixianliu2024@u.northwestern.edu>
2023-02-14 01:34:14 +0000

    maybe

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -3,9 +3,17 @@ class CompaniesController < ApplicationController
   # TODO:
   # def index
   # end
+  def index
+    # get any data in
+    @companies = Company.all
+    # handle business logic
+    # render html file views/companies/index.html.erb
+    
+  end
 
   def show
     @company = Company.find_by({ "id" => params["id"] })
+    # render html file views/companies/show
   end
 
 end
